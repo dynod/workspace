6136f2a0acbeefa87e526d7dab78d2a19bae5633
David Pochet <david.pochet@gmail.com>
2020-04-24 17:28:22 +0200

    Minor tweak to let some projects override PYTHON_FOR_VENV

--- main.mk
+++ main.mk
@@ -2,7 +2,7 @@
 DEVENV_ROOT := $(WORKSPACE_ROOT)/tools/devenv
 
 # All python projects shall work with python 3.8
-PYTHON_FOR_VENV := python3.8
+PYTHON_FOR_VENV ?= python3.8
 
 # devenv makefile suite - defs
 include $(DEVENV_ROOT)/main.mk
