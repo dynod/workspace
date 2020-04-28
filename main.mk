ae5dff33004f80eaf06fe867193a6a3030a42269
David Pochet <david.pochet@gmail.com>
2020-04-28 17:39:05 +0200

    Add dependencies definitions

--- main.mk
+++ main.mk
@@ -1,6 +1,9 @@
 # Root for devenv
 DEVENV_ROOT := $(WORKSPACE_ROOT)/tools/devenv
 
+# Root for workspace definitions
+WORKSPACE_PROJECT_ROOT := $(WORKSPACE_ROOT)/.workspace
+
 # All python projects shall work with python 3.8
 PYTHON_FOR_VENV ?= python3.8
 
